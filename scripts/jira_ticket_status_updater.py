e06e50c05a32ef6c6332dec3c9c049229c39cd85
Basavaraj Sullad <basavaraj.sullad@dentsu.com>
2026-05-03 13:12:44 +0530

    added try exectp block:

--- scripts/jira_ticket_status_updater.py
+++ scripts/jira_ticket_status_updater.py
@@ -1,11 +1,23 @@
-import os, sys
+import os
+import sys
+import logging
 
 workspace_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
 sys.path.insert(0, workspace_root)
 
 from src.plugins.jira_ticketing import ticket_creator
 
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
 ticket_key = os.getenv("JIRA_KEY")
 status = os.getenv("TICKET_STATUS")
 
-ticket_creator.change_ticket_status(ticket_key, status)
+if not ticket_key or not status:
+    logging.error("Missing JIRA_KEY or TICKET_STATUS environment variables")
+    sys.exit(1)
+
+try:
+    ticket_creator.change_ticket_status(ticket_key, status)
+    logging.info(f"Ticket ({ticket_key}) status has been updated to {status}")
+except Exception as e:
+    logging.error(f"Error occurred while updating the Jira ticket {ticket_key}: {e}")
